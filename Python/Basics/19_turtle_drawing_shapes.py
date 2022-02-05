a8da2ceda5e86962ab8a9fdb9c507dd4b5f28aeb
remenraj <remenraj@gmail.com>
2022-02-05 20:55:29 +0530

    Add drawing shapes using turtle module

---
+++ Python/Basics/19_turtle_drawing_shapes.py
@@ -0,0 +1,52 @@
+# turtle program that draws a triangle up to decagon
+# And changes its color after drawing a shape using turtle module
+
+from turtle import Turtle, Screen
+import random
+
+# function to return a random color
+def changecolor():
+
+    R = random.randrange(0, 255, 10)
+    G = random.randrange(0, 255, 10)
+    B = random.randrange(0, 255, 10)
+
+    return R, G, B
+
+
+def drawshapes():
+    #
+    turtly = Turtle()
+    my_screen = Screen()
+
+    # changing the shape to turtle
+    turtly.shape("turtle")
+
+    # setting the colormode of screen
+    my_screen.colormode(255)
+
+    # change the turtle speed
+    turtly.speed(1)
+
+    # initialize sides
+    sides = 3
+
+    # loop to draw shapes
+    for i in range(8):
+
+        # changing the color of turtle
+        turtly.color(changecolor())
+
+        # drawing shapes
+        for j in range(sides):
+            turtly.forward(50)
+            turtly.right(360 / sides)
+
+        # increasing the number of sides of the shape by one
+        sides += 1
+
+    # not closing the screen till mouse click
+    my_screen.exitonclick()
+
+
+drawshapes()
