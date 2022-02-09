dd4db7ce6e14383e88b332119eb5e2f0776f48d6
remenraj <remenraj@gmail.com>
2022-02-09 23:17:12 +0530

    Update puzzle link

--- Python/0-20/7_reeborgs_world_hurdle_4.py
+++ Python/0-20/7_reeborgs_world_hurdle_4.py
@@ -1,11 +1,13 @@
-# solution for hurdle 4 
-# link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Home%204&url=worlds%2Ftutorial_en%2Fhome4.json
+# solution for hurdle 4
+# link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
 
 # function defining code for turning right
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
+
 
 # function to jump over a wall
 def jump():
@@ -19,28 +21,14 @@ def jump():
         move()
     turn_left()
 
-''' function for jumping over a wall when wall_on_right() is not available
-    (I didn't see that function at first)
-def jump():
-    turn_left()
-    move()
-    turn_right()
-    if wall_in_front() == True:
-        jump()
-    else:
-        move()
-        turn_right()
-        while wall_in_front() == False:
-            move()
-        turn_left()
-'''
+
 # main code for reaching the goal
 while at_goal() == False:
-    
+
     # calling function to jump over the wall in front
     while wall_in_front() == True:
         jump()
-    
+
     # checking if the front is clear to move and also if the goal is achieved
     while front_is_clear() == True and at_goal() == False:
         move()
