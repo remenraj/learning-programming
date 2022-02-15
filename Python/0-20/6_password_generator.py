c599046319d407f33e879e1a151e7bc9f89fc2ed
remenraj <remenraj@gmail.com>
2022-02-15 19:45:18 +0530

    Fix typo

--- Python/0-20/6_password_generator.py
+++ Python/0-20/6_password_generator.py
@@ -1,23 +1,76 @@
-#Password Generator Project
+# Password Generator Project
 import random
 
 # list of alphabets
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
 
 # list of numbers
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-#list of symbols
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# list of symbols
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 # welcome message
 print("Welcome to the PyPassword Generator!")
 # prompting the user for various input
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-''' Easy level code. Order not randomised:
+""" Easy level code. Order not randomized:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 # initializing the password
@@ -37,10 +90,10 @@ for n in range(0, nr_numbers):
 
 #printing the password
 print(f"Your password is {password}")
-'''
+"""
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+# Hard Level - Order of characters randomized:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
 # initializing the password list
 password_list = []
@@ -66,5 +119,5 @@ for char in password_list:
     password += char
 
 
-#printing the password
+# printing the password
 print(f"Your password (hard level) is {password}")
