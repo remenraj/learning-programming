307bd62bc425ab3ec1d4a9a2ea2e941eddf8836f
remenraj <remenraj@gmail.com>
2022-02-21 00:05:35 +0530

    Fix start button not showing bug

--- Python/21-50/36-pomodoro-app/main.py
+++ Python/21-50/36-pomodoro-app/main.py
@@ -26,16 +26,20 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+# for counting the number of times the user has completed a session
 reps = 1
 timer = None
 
 # tomato image file directory
-TOMATO_FILE_DIR = r"C:\Users\Cylon\OneDrive\Programming\git\Workspace\100-days-of-code\day-28-pomodoro-app\tomato.png"
+TOMATO_FILE_DIR = r"learning-programming\Python\21-50\36-pomodoro-app\tomato.png"
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 
 def reset_timer():
+    # make the start button visible
+    start_button.grid()
+    
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
     timer_label.config(text="Timer", fg=GREEN)
@@ -50,7 +54,9 @@ def reset_timer():
 
 def start_timer():
     global reps
-    start_button.grid_forget()
+    # make the start button invisible
+    start_button.grid_remove()
+    
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
@@ -103,7 +109,12 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 # setting up tomato as background image
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file=TOMATO_FILE_DIR)
+
+try:
+    tomato_img = PhotoImage(file=TOMATO_FILE_DIR)
+except FileNotFoundError:
+    print("Tomato image file not found")
+    
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(
     100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
