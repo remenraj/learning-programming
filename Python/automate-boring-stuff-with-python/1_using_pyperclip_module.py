ce52464f459af923c12748f894091c8d8e76d09d
remenraj <remenraj@gmail.com>
2022-04-01 01:37:19 +0530

    Add exception handling

--- Python/automate-boring-stuff-with-python/1_using_pyperclip_module.py
+++
@@ -1,7 +0,0 @@
-import pyperclip
-
-# copies text to clipboard
-pyperclip.copy("Hello world!")
-
-# pastes text from clipboard
-print(pyperclip.paste())
--- Python/automate-boring-stuff-with-python/2_different_print_statements.py
+++
@@ -1,6 +0,0 @@
-# prints words with -- between them
-print("Cats", "Dogs", "Frogs", sep="--")
-
-# prints helloworld without space
-print("Hello", end="")
-print("World")
